061826f5adadd521c00657a14b5a067d3cb64565
Hannah Ludema <hel7@calvin.edu>
2016-09-23 14:52:22 -0400

    Initial commit

---
+++ app/src/main/java/edu/calvin/hel7/lab01/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.calvin.hel7.lab01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText( this, "hel7", Toast.LENGTH_LONG ).show();
+    }
+}
